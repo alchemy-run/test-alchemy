4c123a1871f47b517edcccbb590106827fe2ecc0
Sam Goodwin <sam@alchemy.run>
2026-07-25 03:18:51 -0700

    Fix: change head -20 to head -10 for correct word frequency count

--- scripts/word-freq.sh
+++ scripts/word-freq.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find docs -name '*.md' -type f | xargs cat | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | grep -v '^$' | sort | uniq -c | sort -rn | head -20
+find docs -name '*.md' -type f | xargs cat | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | grep -v '^$' | sort | uniq -c | sort -rn | head -10
